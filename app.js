74cb7ce72236d9a5f878f2720edda82a7af5cc2f
Shikhar <shikharg21@gmail.com>
2019-08-11 18:45:28 +0530

    Added Completed JS
    Signed-off-by: Shikhar <shikharg21@gmail.com>

--- app.js
+++ app.js
@@ -1,8 +1,23 @@
 $(() => {
-    let input = $('#input').val()
 
-    $('#addbtn').click(()=>{    
-        console.log(input)
+    let list = $('#list')
+
+    $('#addbtn').click(() => {
+        let input = $('#input').val()
+        list.append(`<li class="list-group-item">${input}</li>`)
+    })
+
+    list.click((event) => {
+        event.target.setAttribute('style', "background-color:lightblue;text-decoration:line-through")
+        event.target.setAttribute('class', "list-group-item taken")
+    })
+
+    // $('.list-group-item.taken').click(() => {
+    //     console.log('clicked')
+    // })
+
+    $('#delbtn').click(() => {
+        $('.taken').remove()
     })
 
 })
